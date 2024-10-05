749d5fdaab8beee37cd4dee27330d86d07f1740c
Pratik4230 <pratikjadhav1438@gmail.com>
2024-10-05 10:50:48 +0530

    user channel + watch history

--- src/controllers/userAuth.js
+++ src/controllers/userAuth.js
@@ -361,4 +361,130 @@ const updateCoverImage =  async (req, res) => {
 }
 
 
-module.exports={signUpUser, logInUser,logoutUser,renewAccess, getProfile , updatePassword, updateAvatarImage, updateCoverImage};
+const getUserChannel = async(req, res) => {
+    const {channelName} = req.params
+
+    if (!username?.trim()) {
+       return res.send("username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                channelName: channelName.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                SubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                channelName: 1,
+                userName: 1,
+                subscribersCount: 1,
+                SubscribedToCount: 1,
+                isSubscribed: 1,
+                avatarImage: 1,
+                coverImage: 1
+            }
+        }
+    ])
+
+    if (!channel?.length) {
+        return res.send("channel doesn't exists")
+    }
+
+    return res
+    .status(200)
+    .json({
+        message:  " success",
+        data: channel[0]
+    })
+    
+}
+
+
+
+const getWatchHistory = async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        channelName: 1,
+                                        userName: 1,
+                                        avatarImage: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200).json({
+        message: "Success",
+        data: user[0].watchHistory,
+    }
+    )
+    
+}
+
+
+module.exports={signUpUser, logInUser,logoutUser,renewAccess, getProfile , updatePassword, updateAvatarImage, updateCoverImage, getUserChannel , getWatchHistory};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 const { 
-    signUpUser, logInUser,logoutUser,renewAccess, getProfile , updatePassword, updateAvatarImage, updateCoverImage
+    signUpUser, logInUser,logoutUser,renewAccess, getProfile , updatePassword, updateAvatarImage, updateCoverImage, getUserChannel , getWatchHistory
 } = require("../controllers/userAuth.js");
 
 const {upload}  = require("../middlewares/multer.js")
@@ -21,6 +21,9 @@ router.route("/renewAccess").post(authUser , renewAccess)
 router.route("/profile" ).get(authUser, getProfile)
 router.route("/updateavatarimage" ).patch(authUser, upload.single("avatar"), updateAvatarImage)
 router.route("/updatecoverimage").patch(authUser, upload.single("coverImage"), updateCoverImage)
+
+router.route("/:channelName").get(authUser , getUserChannel )
+router.route("/watchhistory").get(authUser , getWatchHistory)
 
 
 
