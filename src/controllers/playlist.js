05b536ad42e4f8c09cc99af590311d7db8f68bf3
Pratik4230 <pratikjadhav1438@gmail.com>
2024-10-06 20:26:23 +0530

    added feature of deletePlaylist, add video in playlist and remove video from playlist

--- src/controllers/playlist.js
+++ src/controllers/playlist.js
@@ -1,5 +1,6 @@
 const { isValidObjectId } = require("mongoose");
 const { Playlist } = require("../models/playlistModel");
+const { Video } = require("../models/videoModel");
 
 
 
@@ -92,4 +93,123 @@ const createPlaylist = async(req,res) => {
     }
 
  }
- module.exports={createPlaylist , updatePlaylist}
+
+ const deletePlaylist = async (req, res) => {
+    try {
+        const {playlistId} = req.params;
+    
+        if (!isValidObjectId(playlistId)) {
+            return res.send("invalid playlist id")
+        }
+    
+      const playlist = await Playlist.findById(playlistId)
+    
+      if (!playlist) {
+        return res.send("playlist not found")
+      }
+    
+      if (playlist.owner?.toString() !== req.user?._id.toString()) {
+        return res.send("Only owner can delete Playlist")
+      }
+    
+     const deleted = await Playlist.findByIdAndDelete(playlistId);
+    
+     if (!deleted) {
+        return res.send("Failed to delete Please try again")
+     }
+    
+     return res.status(200).send("Playlist deleted successfully")
+    
+    } catch (error) {
+        console.log("Error delete playlist : " ,error);
+        return res.send("Error delete playlist : " + error)
+    }
+ }
+
+ const addVideoInPlaylist = async(req,res) => {
+    try {
+        const {playlistId, videoId} = req.params;
+    
+        if (!isValidObjectId(playlistId)) {
+            return res.send("invalid playlist id")
+        }
+        if (!isValidObjectId(videoId)) {
+            return res.send("invalid video id")
+        }
+    
+        const playlist = await Playlist.findById(playlistId);
+        const video = await Video.findById(videoId);
+    
+        if (!playlist) {
+            return res.send("Playlist not found")
+        }
+        if (!video) {
+            return res.send("video not found")
+        }
+    
+        if (playlist.owner?.toString() && video.owner?.toString() !== req.user?._id.toString()) {
+            return res.send("only owner can add video in the playlist")
+        }
+    
+        const updatedPlaylist = await Playlist.findByIdAndUpdate(
+            {_id: playlist?._id},
+            {$addToSet: {videos: videoId}},
+            {new:true}
+        )
+    
+        if (!updatedPlaylist) {
+           return res.send("failed to add video in playlist Please try Again") 
+        }
+    
+        return res.status(200).json({
+            message: "video added in playlist successfully",
+            updatedPlaylist
+        })
+    } catch (error) {
+        console.log("error adding video in Playlist : " , error);
+        return res.send("error adding video in Playlist : " + error)
+    }
+ }
+
+ const removeVideoFromPlaylist = async(req,res) => {
+    const {playlistId, videoId} = req.params;
+
+    if (!isValidObjectId(playlistId)) {
+        return res.send("invalid playlist id")
+    }
+    if (!isValidObjectId(videoId)) {
+        return res.send("invalid video id")
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+    const video = await Video.findById(videoId);
+
+    if (!playlist) {
+        return res.send("Playlist not found")
+    }
+    if (!video) {
+        return res.send("video not found")
+    }
+
+    if (playlist.owner?.toString() && video.owner?.toString() !== req.user?._id.toString()) {
+        return res.send("only owner can add video in the playlist")
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        {_id: playlist?._id},
+        {$pull: {videos: videoId}},
+        {new:true}
+    )
+
+    if (!updatedPlaylist) {
+       return res.send("failed to add video in playlist Please try Again") 
+    }
+
+    return res.status(200).json({
+        message: "video added in playlist successfully",
+        updatedPlaylist
+    })
+ }
+
+
+ module.exports={createPlaylist , updatePlaylist, deletePlaylist, addVideoInPlaylist, removeVideoFromPlaylist}
--- src/routes/playlistRoutes.js
+++ src/routes/playlistRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const {authUser} = require("../middlewares/authCheck")
 
-const {createPlaylist, updatePlaylist} = require("../controllers/playlist")
+const {createPlaylist, updatePlaylist, deletePlaylist, addVideoInPlaylist, removeVideoFromPlaylist} = require("../controllers/playlist")
 
 router.route("/create").post(authUser, createPlaylist)
 router.route("/update/:playlistId").patch(authUser, updatePlaylist)
+router.route("/delete/:playlistId").delete(authUser, deletePlaylist)
+router.route("/addvideo/:playlistId/:videoId").post(authUser, addVideoInPlaylist)
+router.route("/removevideo/:playlistId/:videoId").post(authUser, removeVideoFromPlaylist)
 
 module.exports=router
