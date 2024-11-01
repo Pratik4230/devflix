12863e5f990a1cf57f1c477e6606e2b3d57ce2a2
Pratik4230 <pratikjadhav1438@gmail.com>
2024-11-01 09:47:31 +0530

    minor update

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 
 
-app.use(express.json({ limit: "50mb" }));  
+app.use(express.json({ limit: "100mb" }));  
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 
