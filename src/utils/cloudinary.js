8a3c502e07b0e92b4e1b3add06f1d6c376ed59bf
Pratik4230 <pratikjadhav1438@gmail.com>
2024-11-19 16:23:19 +0530

    upload minor change

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,57 +1,54 @@
-const cloudinary = require('cloudinary').v2;
-const fs = require('fs');
-require('dotenv').config()
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+require("dotenv").config();
 
-
-
-cloudinary.config({ 
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME , 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const cloudinaryUpload = async (localFilePath) => {
-    try {
-        if (!localFilePath) {
-            return null;
-        }
-        const uploadResult  = await cloudinary.uploader.upload( localFilePath , {
-     resource_type: "auto",
-         })
-     console.log("file uploaded on cloudinary URL  ");
-        
-     fs.unlink(localFilePath , (error) =>{
-        if (error) {
-            console.warn("Error deleting local file", error);
-        }
-     })
-        return uploadResult;
-
-    } catch (error) {
-        fs.unlink(localFilePath, (err) => {
-            if (err) {
-                
-                console.error("Error deleting local file after failure", err);
-            }
-        });
-
-        return null;
+  try {
+    if (!localFilePath) {
+      return null;
     }
-}
+    const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file uploaded on cloudinary URL  ");
 
-const cloudinaryDelete = async (public_id, resource_type="image") => {
-    try {
-        if (!public_id) return null;
+    //  fs.unlink(localFilePath , (error) =>{
+    //     if (error) {
+    //         console.warn("Error deleting local file", error);
+    //     }
+    //  })
+    return uploadResult;
+  } catch (error) {
+    console.log(error);
 
-        
-        const result = await cloudinary.uploader.destroy(public_id, {
-            resource_type: `${resource_type}`
-        });
-    } catch (error) {
-        console.log("delete on cloudinary failed", error);
-        return error;
-        
-    }
+    // fs.unlink(localFilePath, (err) => {
+    //     if (err) {
+
+    //         console.error("Error deleting local file after failure", err);
+    //     }
+    // });
+
+    return null;
+  }
 };
 
-module.exports={cloudinaryUpload, cloudinaryDelete}
+const cloudinaryDelete = async (public_id, resource_type = "image") => {
+  try {
+    if (!public_id) return null;
+
+    const result = await cloudinary.uploader.destroy(public_id, {
+      resource_type: `${resource_type}`,
+    });
+  } catch (error) {
+    console.log("delete on cloudinary failed", error);
+    return error;
+  }
+};
+
+module.exports = { cloudinaryUpload, cloudinaryDelete };
